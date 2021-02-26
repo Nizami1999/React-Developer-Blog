85d29536d3761dccfa3596b6221cb252070b0766
Nizami <61557989+Nizami1999@users.noreply.github.com>
2021-02-26 16:55:22 +0400

    updated dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,13 @@ import s from './Dialogs.module.scss';
 const Dialogs = () => {
   return (
     <div>
-        <h1 className={s.title}>My chats</h1>
+      <h1 className={s.title}>My chats</h1>
+      <ul className={s.list}>
+        <li className={s.listItem}><a href="#">Nizami ALimammadov</a></li>
+        <li className={s.listItem}><a href="#">Farid Eyvazov</a></li>
+        <li className={s.listItem}><a href="#">Habib Sadiqzade</a></li>
+        <li className={s.listItem}><a href="#">Perviz Namazli</a></li>
+      </ul>
     </div>
   )
 }
